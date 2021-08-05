51037f5742bdad0d46bde7d4a8d447c08c33e369
Usama Bin Sadiq <usama.gormani8300@gmail.com>
2021-08-05 20:51:44 +0500

    2nd Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hello World</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
