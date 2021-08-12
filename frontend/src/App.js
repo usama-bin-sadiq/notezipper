b268b68044b1e4e0331248909ba8338aa4597fbe
Usama Bin Sadiq <usama.gormani8300@github.com>
2021-08-12 23:04:09 +0500

    Landing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { LandingPage } from './components/screens/LandingPage/LandingPage';
 
 
 //function App() {
@@ -17,8 +18,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <main style ={{minHeight: "93vh"}}>
-
+      <main>
+        <LandingPage />
       </main>
       <Footer />
 
---
+++ frontend/src/components/screens/LandingPage/LandingPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './LandingPage.css';
+import { Button, Container, Row } from 'react-bootstrap'
+export const LandingPage = () => {
+    return (
+      <div className="main">
+        <Container>
+          <Row>
+            <div className="intro-text">
+              <div>
+                <h1 className="title">Welcome to Note Zipper</h1>
+                <p className="subtitle">One Safe Place for all your notes</p>
+              </div>
+              <div className="buttonContainer">
+                <a href="/login">
+                  <Button size="lg" className="landingbutton">
+                    Login
+                  </Button>
+                </a>
+                <a href="/register">
+                  <Button size="lg" className="landingbutton" variant = "outline-primary">
+                    Signup
+                  </Button>
+                </a>
+              </div>
+            </div>
+          </Row>
+        </Container>
+      </div>
+    );
+}
