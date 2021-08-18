8e3eb5d01985793f282b54a6deee2bcbb67e1d41
Usama Bin Sadiq <usama.gormani8300@github.com>
2021-08-18 21:10:18 +0500

    Local Mongo DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb://localhost:27017/notezipper",
+      // process.env.MONGO_URI,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
